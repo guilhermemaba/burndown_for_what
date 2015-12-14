50acc6f2e24312fec17c5b64fd9b616519a0db1f
Eduardo Orige <eduardo.orige@gmail.com>
2015-12-14 13:43:37 -0200

    wip #3: start new template. base.html updated

--- burndown_for_what/urls.py
+++ burndown_for_what/urls.py
@@ -1,12 +1,14 @@
 # -*- coding: utf8 -*-
 # vim: ts=4 sts=4 sw=4 et:
 
-from django.conf.urls import include, url
-from django.contrib import admin
-from burndown_for_what.views import BurndownTemplateView
+from django.conf.urls import url
+from django.views.generic import TemplateView
+from burndown_for_what.views import BurndownTemplateView, SprintListView
 
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='burndown.html'), name='home'),
+    url(r'^sprint/$', SprintListView.as_view(), name='sprint_list'),
     url(
         r'^sprint/(?P<sprint_id>\d+)/$',
         BurndownTemplateView.as_view(),
--- burndown_for_what/views.py
+++ burndown_for_what/views.py
@@ -1,17 +1,29 @@
 # -*- coding: utf8 -*-
 # vim: ts=4 sts=4 sw=4 et:
 
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 from burndown_for_what.models import Sprint
 
 
+class SprintListView(ListView):
+    model = Sprint
+    template_name = 'sprint/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SprintListView, self).get_context_data(**kwargs)
+        context['sprints'] = 'active'
+
+        return context
+
+
 class BurndownTemplateView(TemplateView):
-    template_name = 'burndown.html'
+    template_name = 'sprint/burndown.html'
 
     def get_context_data(self, **kwargs):
         sprint = Sprint.objects.get(pk=kwargs.get('sprint_id'))
         context = super(BurndownTemplateView, self).get_context_data(**kwargs)
+        context['sprints'] = 'active'
+
         context['team'] = sprint.team.name
         context['name'] = sprint.name
         context['resume'] = sprint.resume
